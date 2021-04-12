309c92845417ce4d41fdda8ddad5f5763e326e07
Ruslan <rus@gmx.com>
2021-04-12 19:00:10 +0100

    Init Tasks.jsx

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,23 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Do 1',
+        day: 'Mon'
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Do 2',
+        day: 'Tue',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Do 3',
+        day: 'Fri',
+        reminder: false
+    },
+];
+export const Tasks = () => {
+  return <div></div>;
+};
