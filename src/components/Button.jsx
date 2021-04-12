e5fa868d40d23bc79c08977822b8eaf0e235a40c
Ruslan <rus@gmx.com>
2021-04-12 18:43:44 +0100

    Add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+export const Button = ({ color, text }) => {
+  return (
+    <button style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+
+export default Button;
