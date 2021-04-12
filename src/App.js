0bc48f9390db699b8d4dce4f671971a04634528f
Ruslan <rus@gmx.com>
2021-04-12 10:38:33 +0100

    Add Header.jsx boiler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const name = 'Tom';
+
   return (
-    <div className="App">
-      <h1>Hello from React</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
