0c48b3952b86a53756c916e0d225dd16238b2b37
Ruslan <rus@gmx.com>
2021-04-12 21:21:17 +0100

    map through Tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,23 +1,29 @@
 const tasks = [
-    {
-        id: 1,
-        text: 'Do 1',
-        day: 'Mon'
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Do 2',
-        day: 'Tue',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Do 3',
-        day: 'Fri',
-        reminder: false
-    },
+  {
+    id: 1,
+    text: 'Do 1',
+    day: 'Mon',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Do 2',
+    day: 'Tue',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Do 3',
+    day: 'Fri',
+    reminder: false,
+  },
 ];
 export const Tasks = () => {
-  return <div></div>;
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3>{task.text}</h3>
+      ))}
+    </>
+  );
 };
