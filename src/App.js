b327784b26385bb7f4137709e41b08bccbacbf18
Ruslan <rus@gmx.com>
2021-04-13 21:36:41 +0100

    Activate delete

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
   //delete task
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id));
   };
   return (
     <div className="container">
