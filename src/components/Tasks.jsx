f1b37369ac48a8cd2819afd41c8475912e75af1e
Ruslan <rus@gmx.com>
2021-04-13 19:07:51 +0100

    Refactor Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,24 +1,4 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Do 1',
-    day: 'Mon',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Do 2',
-    day: 'Tue',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Do 3',
-    day: 'Fri',
-    reminder: false,
-  },
-];
-export const Tasks = () => {
+export const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
