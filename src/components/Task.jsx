e4d9fef821080c80bdf0508c08ae80f8446e2be7
Ruslan <rus@gmx.com>
2021-04-15 23:54:38 +0100

    Add toggleReminder color response

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,9 @@ import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task" onDoubleClick={(id) => onToggle(task.id)}>
+    <div
+      className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={(id) => onToggle(task.id)}>
       <h3>
         {task.text}{' '}
         <FaTimes
