a5a88f067995e32828fa47159b1f4242220765c8
Ruslan <rus@gmx.com>
2021-04-16 20:33:35 +0100

    Add abilitty to add new tasks

--- src/App.js
+++ src/App.js
@@ -25,8 +25,14 @@ function App() {
       reminder: false,
     },
   ]);
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
-  //delete task
+  //Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -42,7 +48,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
 
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
 
-export const AddTask = () => {
+export const AddTask = ({ onAdd }) => {
   const [text, setText] = useState('');
   const [day, setDay] = useState('');
   const [reminder, setReminder] = useState(false);
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert('Please add a task');
+      return;
+    }
+    onAdd({ text, day, reminder });
+    setText('');
+    setDay('');
+    setReminder(false);
+  };
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Task</label>
         <input
@@ -31,6 +43,7 @@ export const AddTask = () => {
         <label>Set Reminder</label>
         <input
           type="checkbox"
+          checked={reminder}
           value={reminder}
           onChange={(e) => setReminder(e.currentTarget.checked)}
         />
